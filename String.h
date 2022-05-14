0d85c2ffcd13ce45750d12539a857adc31bc48ab
PLLX76 <84037558+PLLX76@users.noreply.github.com>
2022-05-14 12:50:54 +0200

    Delete String.h

--- String.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-
-std::string AddString(std::string string1, std::string string2, std::string string3) {
-	return string1 + string2 + string3;
-}
